b0758f0c859632ae08fde9b2cc31a158f4b95e91
Minhduc027 <minhduylongthuan@gmail>
2024-07-20 00:29:02 +0700

    final

--- Assets/Script/Enemies/Grape.cs
+++ Assets/Script/Enemies/Grape.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Grape : MonoBehaviour, IEnemy
 {
     [SerializeField] private GameObject grapeProjectilePrefab;
-
+    [SerializeField] private float duration = 2f;
+    [SerializeField] private float spreadAngle = 15f;
     private Animator myAnimator;
     private SpriteRenderer spriteRenderer;
 
@@ -25,7 +26,24 @@ public class Grape : MonoBehaviour, IEnemy
             spriteRenderer.flipX = true;
         }
     }
+    private void SpawnFanShapeProjectiles(Vector3 targetPosition)
+    {
+        // Spawn 3 projectiles in a fan-shaped pattern
+        for (int i = -1; i <= 1; i++)
+        {
+            float angle = i * spreadAngle * Mathf.Deg2Rad;
+            Vector3 spawnPosition = transform.position;
+            Vector3 direction = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0f);
+            GameObject projectile = Instantiate(grapeProjectilePrefab, spawnPosition, Quaternion.LookRotation(direction));
+            StartCoroutine(DestroyProjectileAfterDuration(projectile, duration));
+        }
+    }
 
+    private IEnumerator DestroyProjectileAfterDuration(GameObject projectile, float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        Destroy(projectile);
+    }
     public void SpawnProjectileAnimEvent() {
         Instantiate(grapeProjectilePrefab, transform.position, Quaternion.identity);
     }
